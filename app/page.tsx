7e00d81fd9ddf091f2c14c6adfe6a1f71988b725
Allan Kiche <tle2819@students.mu.ac.ke>
2023-08-24 16:47:52 +0300

    track

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function page() {
   return (
     <div className='w-screen h-screen bg-black flex justify-center items-center text-white'>
-      <div className='w-full max-w-[600px]'>
-        <h1 className='text-6xl'>title</h1>
-        <p>description</p>
+      <div className='w-full max-w-[600px] mx-auto'>
+        <h1 className='text-6xl'>The best Journal App</h1>
+        <p className='text-2xl text-white/60'> This is the best app to track your mood throughout your life. All you have to do is to be honest</p>
         <div>
           <button>Get Started</button>
         </div>
