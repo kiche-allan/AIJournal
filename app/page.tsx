f04c9108f6bacbc18bbcd6bdbb79e20a744c4459
Allan Kiche <tle2819@students.mu.ac.ke>
2023-08-24 16:45:49 +0300

    next1

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>Home</div>
+    <div className='w-screen h-screen bg-black flex justify-center items-center text-white'>
+      <div className='w-full max-w-[600px]'>
+        <h1 className='text-6xl'>title</h1>
+        <p>description</p>
+        <div>
+          <button>Get Started</button>
+        </div>
+      </div>
+    </div>
+    
+
   )
 }
