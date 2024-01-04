ac5468e269b67dc98928483b1c1a6dc89ad61ed7
Allan Kiche <tle2819@students.mu.ac.ke>
2024-01-04 12:54:22 +0300

    auth

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,3 +1,17 @@
+
+const getEntries = async () => {
+    const user = await currentUser()
+    const entries = await prisma.entry.findMany({
+        where: {
+            userId: user.id as string,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        },
+    })
+    })
+
+}
 const JournalPage = () => {
     return <div>Journal</div>
     
